e88714835e22748fb6c2851762d46d794bc92797
Hanyoung Park <85710397+Hanyoung-Park@users.noreply.github.com>
2023-03-15 11:12:16 +0900

    Update worldtime.py

--- worldtime.py
+++ worldtime.py
@@ -1,25 +1,47 @@
 import discord
-from discord.ext.commands import Bot
-import datetime
+import pytz
+from datetime import datetime
 
-TOKEN = ''
 
-intents = discord.Intents.default()
+TOKEN = ""
+client = discord.Client()
 
-# !로 시작하면 명령어로 인식
-bot = Bot(command_prefix='worldt', intents=intents)
+timezones = {
+    "한국": "Asia/Seoul",
+    "미국 동부": "America/New_York",
+    "미국 서부": "America/Los_Angeles",
+    "일본": "Asia/Tokyo",
+    "중국": "Asia/Shanghai",
+    "러시아": "Europe/Moscow",
+    "영국": "Europe/London",
+    "독일": "Europe/Berlin",
+    "네덜란드": "Europe/Berlin",
+    "오스트레일리아": "Australia/Sydney",
+    "뉴질랜드": "Pacific/Auckland"
+}
 
-# !hello 명령어 처리
-@bot.command()
-async def ime(ctx, country):
-  
+@client.event
+async def on_ready():
+    print("세계시간 봇 작동 시작")
 
-  if country in
-  await ctx.reply('{0}의 시간은 현재 {1} 입니다.'.format(country, worldtime)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-# !bye 명령어 처리
-@bot.command()
-async def bye(ctx):
-  await ctx.reply('See you later!')
+    if message.content.startswith("!time"):
+        timezone_str = message.content[6:].strip()
 
-bot.run(TOKEN)
+        if timezone_str not in timezones:
+            await message.channel.send("시간대를 찾을 수 없습니다.")
+            return
+
+        timezone = pytz.timezone(timezones[timezone_str])
+        now = datetime.now(timezone)
+        time_str = now.strftime("%Y년 %m월 %d일 %H시 %M분 %S초")
+
+        response = f"{timezone_str} 시간은 {time_str}입니다."
+        await message.channel.send(response)
+
+client.run(TOKEN)
+
